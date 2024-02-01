41ad2d1573a2c694f92604a818431a53d4bf5d1b
chintanpatel <chintanpatel2013@icloud.com>
2024-02-01 11:56:51 +0530

    Create User.java bean file for login application.

---
+++ src/main/java/org/chintanpatel/app/bean/User.java
@@ -0,0 +1,80 @@
+package org.chintanpatel.app.bean;
+
+public class User {
+
+    private int userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private long mobile;
+    private String userName;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String email, long mobile, String userName, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.mobile = mobile;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(long mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
